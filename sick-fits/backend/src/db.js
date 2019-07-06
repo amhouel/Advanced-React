7894412afb375f7250584267e61af5c6c97353d9
amhouel <amhouel@gmail.com>
2019-07-06 15:34:37 -0400

    server errors fixed

--- sick-fits/backend/src/db.js
+++ sick-fits/backend/src/db.js
@@ -1,12 +1,13 @@
 // this file connected to the remote prisma DB and gives us the ability to query it with JS
 
+// This file connects to the remote prisma DB and gives us the ability to query it with JS
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
-    typeDefs: 'src/generated/prisma.graphql',
-    endPoint: process.env.PRISMA_ENDPOINT,
-    secret: process.env.PRISMA_SECRET,
-    debug: false,
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
 });
 
-modules.export = db;
+module.exports = db;
